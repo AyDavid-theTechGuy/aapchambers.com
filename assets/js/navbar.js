863da067910c9b30b61d8e6a2386ba18b8c22659
Sowunmi Ayoola <aydavidfjc@gmail.com>
2025-03-24 13:15:38 -0700

    Update navbar.js

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -15,7 +15,7 @@ nav.innerHTML = `
 			aria-expanded="false"
 			aria-label="Toggle navigation"
 		>
-			<span class="material-icons mi-24">menu</span>
+			<span class="navbar-toggler-icon"></span>
 		</button>
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
 			<ul class="navbar-nav ms-0 ms-auto">
